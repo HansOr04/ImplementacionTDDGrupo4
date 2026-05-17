a6de43d3be26818e760e62e9c6d05ffc554169a8
Estebin21 <esteban.narvaez.acurio@udla.edu.ec>
2026-05-16 21:24:41 -0500

    Ciclo 5: implementar centimetrosAMetros con sus tests

--- src/main/java/ConversorUnidades.java
+++ src/main/java/ConversorUnidades.java
@@ -5,4 +5,8 @@ public class ConversorUnidades{
     public double metrosAMilimetros(double m){
         return m * 1000;
     }
+    public double centimetrosAMetros(double centimetros){
+        final double CENTIMETROS_POR_METRO = 100.0;
+        return centimetros / 100;
+    }
 }
